63c5dc7b8dd7f73c903af41c6f1d69b0a7d36eb2
Mat <mat555333@gmail.com>
2020-07-22 01:23:11 +0200

    man we already did like half the work

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Board : MonoBehaviour
+{
+    public GameObject connectorPrefab;
+    public GameObject diskPrefab;
+    public GameObject columnMask;
+
+    [Range(1, 50)]
+    public int boardColumns = 10;
+
+    [Range(1, 50)]
+    public int boardRows = 10;
+
+    private GameObject[,] disks;
+
+    public GameObject[] masks;
+
+    private void Start()
+    {
+        disks = new GameObject[boardColumns, boardRows];
+        masks = new GameObject[boardColumns];
+        DrawGrid();
+    }
+
+    public void MaskClicked(int index)
+    {
+        Debug.Log($"({index} pressed. {CheckPlacement(index)})");
+        PlaceDisk(index);
+    }
+
+    public void PlaceDisk(int columnRow)
+    {
+        int position = CheckPlacement(columnRow);
+        if (position == -1){
+            return;
+        }
+
+        GameObject disk = Instantiate(diskPrefab, new Vector3(columnRow, position, 0), diskPrefab.transform.rotation);
+        disks[position, columnRow] = disk;
+
+    }
+
+    private int CheckPlacement(int columnRow)
+    {
+        for (int i = 0; i < boardRows; i++)
+        {
+            if (disks[i, columnRow] == null)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void CreateMask(int index)
+    {
+        Vector3 position = new Vector3(index, -0.5f, 0);
+        GameObject mask = Instantiate(columnMask, position, Quaternion.identity, this.transform);
+        // sets mask to be as tall as the board
+        mask.transform.localScale += new Vector3(0, boardRows -1, 0);
+
+        MaskClick tmp = mask.gameObject.transform.GetChild(0).GetComponent<MaskClick>();
+
+        tmp.index = index;
+        tmp.board = this;
+
+        masks[index] = mask;
+    }
+
+
+    private void DrawGrid()
+    {
+        for (int column = 0; column < boardColumns; column++)
+        {
+            CreateMask(column);
+
+            for (int row = 0; row < boardRows; row++)
+            {
+                Vector3 position = new Vector3(row, column, 0);
+                Instantiate(connectorPrefab, position, Quaternion.identity, this.transform);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/MaskClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskClick : MonoBehaviour
+{
+
+    public Board board;
+    public int index;
+
+
+    private void OnMouseEnter()
+    {
+        // show ghost
+    }
+    private void OnMouseExit()
+    {
+        // stop showing ghost
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            board.MaskClicked(index);
+        }
+    }
+
+}
